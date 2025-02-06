d23704ac145a588ef115556cce533f5804843273
Michał Domarus <mdomarus@gmail.com>
2025-02-06 23:13:53 +0100

    WIP

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -1,39 +1,96 @@
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu";
+import { NavigationMenuLink } from "@/components/ui/navigation-menu";
 import { cn } from "@/lib/utils";
+import { MenuIcon } from "lucide-react";
+import { Url } from "next/dist/shared/lib/router/router";
 import Link from "next/link";
 import { forwardRef } from "react";
 
 const GRAPHIQL = process.env.GRAPHIQL;
 
-const projekty: { title: string; href: string; description?: string }[] = [
-  {
-    title: "Aktualne",
-    href: "/projekty/aktualne",
-  },
-  {
-    title: "Zrealizowane",
-    href: "/projekty/zrealizowane",
-  },
-];
+const projektySection: NavigationSection = {
+  title: "Projekty",
+  items: [
+    {
+      title: "Aktualne",
+      href: "/projekty/aktualne",
+    },
+    {
+      title: "Zrealizowane",
+      href: "/projekty/zrealizowane",
+    },
+  ],
+};
 
-const fundacja: { title: string; href: string; description?: string }[] = [
-  {
-    title: "Statut",
-    href: "/fundacja/statut-fundacji",
-  },
-  {
-    title: "Dane urzędowe",
-    href: "/fundacja/dane-urzedowe",
-  },
-];
+const fundacjaSection: NavigationSection = {
+  title: "Fundacja",
+  items: [
+    {
+      title: "Statut",
+      href: "/fundacja/statut-fundacji",
+    },
+    {
+      title: "Dane urzędowe",
+      href: "/fundacja/dane-urzedowe",
+    },
+  ],
+};
+
+interface NavigationItem {
+  title?: string;
+  href?: Url;
+}
+
+interface NavigationSection {
+  title: string;
+  items: NavigationItem[];
+}
+
+const NavigationLink = ({ item }: { item: NavigationItem }) => {
+  if (!item?.href) {
+    return null;
+  }
+
+  return (
+    <li>
+      <Link
+        href={item.href}
+        prefetch
+        className="block my-1 hover:underline font-medium"
+      >
+        {item.title}
+      </Link>
+    </li>
+  );
+};
+
+const Section = ({ section }: { section: NavigationSection }) => {
+  return (
+    <div className="flex flex-col">
+      <div className="peer pb-1 mt-1">{section.title}</div>
+      <ul className="md:invisible md:peer-hover:visible hover:visible">
+        {section.items.map((item) => (
+          <NavigationLink key={item.title} item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const Navigation = () => {
+  return (
+    <nav>
+      <MenuIcon className="block md:hidden" />
+      <ul className="bg-white md:bg-transparent absolute md:relative flex flex-col md:flex-row gap-6 p-6 md:p-0">
+        <NavigationLink item={{ title: "Aktualności", href: "/aktualnosci" }} />
+        <Section section={fundacjaSection} />
+        <Section section={projektySection} />
+        <NavigationLink item={{ title: "GraphiQL", href: GRAPHIQL }} />
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
 
 const ListItem = forwardRef<
   React.ComponentRef<typeof Link>,
@@ -62,54 +119,3 @@ const ListItem = forwardRef<
   );
 });
 ListItem.displayName = "ListItem";
-
-const Navigation = () => {
-  return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <Link href="/aktualnosci" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Aktualności
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Fundacja</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {fundacja.map(({ title, href, description }) => (
-                <ListItem key={title} title={title} href={href}>
-                  {description}
-                </ListItem>
-              ))}
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Projekty</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {projekty.map(({ title, href, description }) => (
-                <ListItem key={title} title={title} href={href}>
-                  {description}
-                </ListItem>
-              ))}
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        {GRAPHIQL && (
-          <NavigationMenuItem>
-            <Link href={GRAPHIQL} target="_blank" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                GraphiQL
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        )}
-      </NavigationMenuList>
-    </NavigationMenu>
-  );
-};
-
-export default Navigation;
